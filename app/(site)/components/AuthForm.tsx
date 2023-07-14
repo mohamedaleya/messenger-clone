ec38a73a7e8a8777cd677f7778836304d248025f
Mohamed Aleya <mohamed.aleya@groupe-telnet.net>
2023-07-14 15:38:04 +0100

    Sidebar, navigation & layout

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,27 +1,36 @@
 'use client';
 
 import clsx from 'clsx';
-import Input from '@/app/components/Inputs/Input';
+import Input from '@/app/components/inputs/Input';
 import Button from '@/app/components/Button';
 import AuthSocialButton from './AuthSocialButton';
 
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useForm, FieldValues, SubmitHandler } from 'react-hook-form';
 
 import { BsGithub, BsGoogle } from 'react-icons/bs';
 import axios from 'axios';
 import toast from 'react-hot-toast';
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
+    const session = useSession();
+    const router = useRouter();
     const [variant, setVariant] = useState<Variant>('LOGIN');
     const [isLoading, setIsLoading] = useState(false);
 
+    useEffect(() => {
+        if (session?.status === 'authenticated') {
+            router.push('/users');
+        }
+    }, [session?.status, router]);
+
     const toggleVariant = useCallback(() => {
         setVariant((prev) => (prev === 'LOGIN' ? 'REGISTER' : 'LOGIN'));
-    }, [variant]);
+    }, []);
 
     const {
         register,
@@ -41,6 +50,7 @@ const AuthForm = () => {
         if (variant === 'REGISTER') {
             axios
                 .post('/api/register', data)
+                .then(() => signIn('credentials', data))
                 .catch(() => toast.error('Something went wrong.'))
                 .finally(() => setIsLoading(false));
         }
@@ -57,6 +67,7 @@ const AuthForm = () => {
 
                     if (callback?.ok && !callback?.error) {
                         toast.success('Successfully logged in.');
+                        router.push('/users');
                     }
                 })
                 .finally(() => setIsLoading(false));
---
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -0,0 +1,5 @@
+const DesktopSidebar = () => {
+    return <div>Desktop Sidebar!</div>;
+};
+
+export default DesktopSidebar;
